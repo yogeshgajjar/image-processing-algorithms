2b3afafff0d0c3287d4dbfe92ec33bdf06201df2
Yogesh Gajjar <yogeshgajjar75@gmail.com>
2020-05-31 01:37:24 -0700

    added new files

---
+++ geometric_image_modification/execute.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dir=$(pwd)
+echo "The working directory is:" $dir
+
+./output_warping ${PWD%%/}/image_warping/images/hedwig.raw ${PWD%%/}/image_warping/images/bb8.raw ${PWD%%/}/image_warping/images/raccoon.raw ${PWD%%/}/image_warping/warped_hedwig.raw ${PWD%%/}/image_warping/warped_bb8.raw ${PWD%%/}/image_warping/warped_raccoon.raw ${PWD%%/}/image_warping/ori_hedwig.raw ${PWD%%/}/image_warping/ori_bb8.raw ${PWD%%/}/image_warping/ori_raccoon.raw 512 512 3
+./output_stiching ${PWD%%/}/image_stiching/images/left.raw ${PWD%%/}/image_stiching/images/middle.raw ${PWD%%/}/image_stiching/images/right.raw ${PWD%%/}/image_stiching/stich.raw 720 480 3 
+make clean
+
+echo "Check the output in respective project folder"
+
